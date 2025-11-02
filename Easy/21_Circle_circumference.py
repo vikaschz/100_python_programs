d684f0a86da244ea1d2984fa7bf5369ebd67c9bd
vikas channaveerashettar <vikasch5623@gmail.com>
2025-11-02 22:49:27 +0530

    Added program 21

---
+++ Easy/21_Circle_circumference.py
@@ -0,0 +1,18 @@
+"""
+Program: Circle circumference
+Description: Compute circumference 2πr, 2 decimal.
+
+Example:
+Input/Output:
+r=3 -> 18.85
+"""
+
+r = int(input("Enter radius: "))
+circumference = 2 * 3.142 * r
+print(f"circumference of circle is: {circumference:.2f}")
+
+
+
+
+
+
