3c6328b464d16ef0a97e08e2c07e297320e0ca15
vikas channaveerashettar <vikasch5623@gmail.com>
2025-11-03 22:38:19 +0530

    Added program 26

---
+++ Easy/26_Lowercase_converter.py
@@ -0,0 +1,15 @@
+"""
+Program: Lowercase converter
+Description: Convert string to lowercase.
+
+Example:
+Input/Output:
+HeLLo -> hello
+"""
+
+text = input("Enter a string: ")
+print(text.lower())
+
+
+
+
