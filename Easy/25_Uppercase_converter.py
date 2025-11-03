ebdaf3b7951f6f6c424c65143c582d47118d6c09
vikas channaveerashettar <vikasch5623@gmail.com>
2025-11-03 22:36:59 +0530

    Added program 25

---
+++ Easy/25_Uppercase_converter.py
@@ -0,0 +1,14 @@
+"""
+Program: Uppercase converter
+Description: Convert string to uppercase.
+
+Example:
+Input/Output:
+hello -> HELLO
+"""
+text = input("Enter a string: ")
+print(text.upper())
+
+
+
+
