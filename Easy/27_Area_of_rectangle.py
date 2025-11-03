1012e31077c52d0692becdb40db6e7962ab43a93
vikas channaveerashettar <vikasch5623@gmail.com>
2025-11-03 22:39:05 +0530

    Added program 27

---
+++ Easy/27_Area_of_rectangle.py
@@ -0,0 +1,15 @@
+"""
+Program: Area of rectangle
+Description: Compute area = l*w.
+
+Example:
+Input/Output:
+5 3 -> 15
+"""
+
+length = float(input("Enter length of rectangle: "))
+breadth = float(input("Enter breadth of rectangle: "))
+area = length * breadth
+print(f"Area of rectangle is: {area}")
+
+
