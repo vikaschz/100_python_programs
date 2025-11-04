64f676e359e2e05406d12944a7ce04add8e92d72
vikas channaveerashettar <vikasch5623@gmail.com>
2025-11-04 22:21:01 +0530

    Added program 31

---
+++ Easy/31_List_sum.py
@@ -0,0 +1,16 @@
+"""
+Program: List sum
+Description: Sum all numbers in a list.
+
+Example:
+Input/Output:
+[1,2,3,4] -> 10
+"""
+Sum = 0
+l = [1,2,3,4]
+for n in l:
+    Sum += n
+print(Sum)
+
+
+
