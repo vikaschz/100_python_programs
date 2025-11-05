dad0ba2f98c23004d99cd89bd9f0f0524e08f6c6
vikas channaveerashettar <vikasch5623@gmail.com>
2025-11-05 22:13:18 +0530

    Added program 41

---
+++ Medium/41_merge_sorted_lists.py
@@ -0,0 +1,18 @@
+"""
+Program: Merge Sorted Lists
+Description: Merge two sorted lists.
+
+
+"""
+
+l1 = [5,7,6,1,4]
+l2 = [9,8,2,10,3]
+
+merged = sorted(l1) + sorted(l2)
+print(sorted(merged))
+
+
+
+
+
+
