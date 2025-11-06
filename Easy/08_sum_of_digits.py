70c77b16a3899438ddd44bc7edb9f6185a7226ed
vikas channaveerashettar <vikasch5623@gmail.com>
2025-11-06 23:33:36 +0530

    updated code

--- Easy/08_sum_of_digits.py
+++ Easy/08_sum_of_digits.py
@@ -4,18 +4,14 @@ Difficulty: Easy
 Description: Sum all digits of the integer.
 """
 
-
-def sum_of_digits(n):
-    temp = n
-    sum = 0
-
-    while temp > 0:
-        digit = temp % 10
-        sum += digit
-        temp //= 10
-
-    return f"Sum of {n} is : {sum}"
-
-
 num = int(input("Enter a number: "))
-print(sum_of_digits(num))
+
+temp = num
+sum = 0
+
+while temp > 0:
+    digit = temp % 10
+    sum += digit
+    temp //= 10
+
+print(f"Sum of {num} is : {sum}")
