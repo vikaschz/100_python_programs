b39b0df9b2e42564b1e1f983f41830aa9ee92122
vikas channaveerashettar <vikasch5623@gmail.com>
2025-11-06 23:29:08 +0530

    Updated code

--- Easy/06_Count_digits.py
+++ Easy/06_Count_digits.py
@@ -4,18 +4,10 @@ Difficulty: Easy
 Description: Given an integer, print number of digits.
 """
 
-def count_digits(n):
-    count = 0
-    while n > 0:
-        n = n // 10      # Remove last digit
-        count += 1       # Increase digit count
-    return count
 
-# Input
-num = int(input("Enter a number: "))
-
-# Edge case: if number is 0, it has 1 digit
-if num == 0:
-    print(1)
-else:
-    print(count_digits(num))
+n = int(input("Enter a number: "))
+count = 0
+while n > 0:
+    n = n // 10     
+    count += 1       
+print(count)
