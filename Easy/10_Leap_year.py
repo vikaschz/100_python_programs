40fd027bc544f40b3d97ec8831c3ec99b4301ff8
vikas channaveerashettar <vikasch5623@gmail.com>
2025-11-07 22:02:11 +0530

    modified program

--- Easy/10_Leap_year.py
+++ Easy/10_Leap_year.py
@@ -1,17 +1,14 @@
 """
 Program: Leap year
-Difficulty: Easy
 Description: Check if a year is leap (Gregorian).
 """
 
-def is_leap_year(year):
-    # Leap year conditions:
-    # 1. Divisible by 4
-    # 2. Not divisible by 100 unless also divisible by 400
-    return (year % 4 == 0) and (year % 100 != 0 or year % 400 == 0)
-
+# Leap year conditions:
+# 1. Divisible by 4
+# 2. Not divisible by 100 unless also divisible by 400
 year = int(input("Enter the year: "))
-if is_leap_year(year):
+if (year % 4 == 0) and (year % 100 != 0 or year % 400 == 0):
     print("Leap Year")
 else:
     print("Not a Leap Year ")
+
