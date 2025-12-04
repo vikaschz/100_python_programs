46a0b628b701361923b8db2bbc05fb91167542ce
vikas channaveerashettar <vikasch5623@gmail.com>
2025-12-04 11:20:05 +0530

    Added OOP examples

---
+++ OOPs with python/class/5.py
@@ -0,0 +1,14 @@
+"""
+5. Write a class Animal with a method sound() that prints “Animal makes sound”.
+Create an object and call the method.
+"""
+
+
+class Animal:
+
+    def sound(self):
+        print("Animal makes sound")
+
+
+a1 = Animal()
+a1.sound()
