97e4b43111b5d0d49619abadd85800006f15c52a
vikas channaveerashettar <vikasch5623@gmail.com>
2025-12-05 14:57:37 +0530

    Added Counter class encapsulation example

---
+++ OOPs_with_python/encapsulation/03_class_Counter.py
@@ -0,0 +1,20 @@
+"""Create a class Counter that keeps a private count variable.
+Add methods to increase and view the count."""
+
+class Counter:
+    def __init__(self):
+        self.__count = 0   # private variable
+
+    def increase(self):
+        self.__count += 1
+
+    def get_count(self):
+        return self.__count
+
+
+c = Counter()
+c.increase()
+c.increase()
+c.increase()
+print(c.get_count())  # 3
+
